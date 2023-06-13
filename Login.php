8d75e49a138d7316d244684c1e164ecb12e73f6b
BaptisteMathon <103061783+BaptisteMathon@users.noreply.github.com>
2023-06-13 15:52:27 +0200

    test

--- Login.php
+++ Login.php
@@ -1,3 +1,36 @@
+<?php 
+    if(count($_POST) != 0){
+        try
+        {
+            $db = new PDO('mysql:host=localhost;dbname=fithome;charset=utf8', 'root', 'root');
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+
+        $sqlQueryInfo = 'SELECT * FROM users';
+        $usersStatement = $db->prepare($sqlQueryInfo);
+        $usersStatement->execute();
+        $users_online = $usersStatement->fetchAll();
+
+        for($i = 0; $i < count($users_online); $i++){
+            if($users_online[$i]['Email'] == $_POST['mailLogin'] && $users_online[$i]['Password'] == $_POST['passwordLogin']){
+
+                $indice = intval($users_online[$i]['user_id']);
+                
+                $sqlLogin = "UPDATE users SET UserOnline = 1 WHERE user_id = $indice";
+                $userEdit = $db->prepare($sqlLogin);
+                $userEdit->execute();
+            } else 
+            {
+                // echo '<br>KO<br>';
+            }
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,16 +45,16 @@
 
     <?php require('.\header.php') ?>
     <div class="centré">
-    <form class="form">
+    <form action="Login.php" method="POST" class="form">
     <p class="title">Login </p>
     <p class="message">signin to your account. </p>
     <label>
-        <input required="" placeholder="" type="email" class="input">
+        <input required="" placeholder="" type="email" class="input" name="mailLogin">
         <span>Email</span>
     </label> 
         
     <label>
-        <input required="" placeholder="" type="password" class="input">
+        <input required="" placeholder="" type="password" class="input" name="passwordLogin">
         <span>Password</span>
     </label>
     <button class="submit">Submit</button>
